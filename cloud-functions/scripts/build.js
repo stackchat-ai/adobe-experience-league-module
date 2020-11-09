b7a39d719cd43ebcf26f964e4ca24a699aa265ee
marcus-robinson <mr.marcus.l.robinson@gmail.com>
2020-11-09 12:02:59 +1100

    Refactor CF build script to work with Windows.

---
+++ cloud-functions/scripts/build.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable security/detect-non-literal-fs-filename */
+const fs = require('fs')
+
+/**
+ * Port of the following npm build script to Node so that it runs on both Windows and Mac
+ * mkdir -p dist && cat build/* > build/_output.js && cat build/_output.js | grep -v '^\\(import \\)' > build/_filtered-output.js && prettier build/_filtered-output.js > dist/cloud-functions.js
+ */
+const distPath = './dist'
+const buildPath = './build'
+
+try {
+  let output = ''
+  fs.mkdirSync(distPath)
+  const files = fs.readdirSync(buildPath)
+  files.forEach(file => {
+    output += fs.readFileSync(`${buildPath}/${file}`)
+  });
+  fs.writeFileSync(`${buildPath}/_filtered-output.js`, output.replace(/^import .*/gm, ''))
+} catch (err) {
+  console.error(`There was an error! ${err}`)
+}
