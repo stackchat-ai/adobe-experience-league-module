dea8f6490998cee15a0ea10fcdab807f350f6b52
marcus-robinson <mr.marcus.l.robinson@gmail.com>
2020-11-09 14:47:56 +1100

    Remove accidental rm's from build.js

--- cloud-functions/scripts/build.js
+++ cloud-functions/scripts/build.js
@@ -12,9 +12,6 @@ const buildPath = './build'
 const prettierOpts = prettier.resolveConfig.sync('.prettierrc.json');
 
 try {
-  fs.rmdirSync(distPath, { recursive: true })
-  fs.rmdirSync(buildPath, { recursive: true })
-
   let output = ''
   fs.mkdirSync(distPath)
   const files = fs.readdirSync(buildPath)
